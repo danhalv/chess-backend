830e537b6bfe79bb77e1e86461c84cc222d8849f
DanielH4 <danne.halvarsson@gmail.com>
2025-01-24 21:17:02 +0100

    add test class for explicit testing of HttpController
    - This is a start towards refactoring endpoint tests.

---
+++ ChessApi.Tests/Controllers/ChessApiHttpControllerTests.cs
@@ -0,0 +1,96 @@
+using System.Linq;
+using System.Net;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using ChessApi.Models;
+using ChessApi.Models.Chess;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace ChessApi.Tests.Controllers;
+
+public class ChessApiHttpControllerTests
+{
+  private readonly HttpClient _client;
+  private readonly TestServer _server;
+
+  public ChessApiHttpControllerTests()
+  {
+    var serverArgs = new string[0];
+    _server = new TestServer(ChessApi.Program.CreateWebHostBuilder(serverArgs));
+    _client = _server.CreateClient();
+  }
+
+  [Fact]
+  public async Task CreateChessGame_Returns_Created_Game()
+  {
+    // Act
+    var response = await _client.PostAsync("/api/chessgames", null);
+
+    // Assert
+    Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+    var responseString = await response.Content.ReadAsStringAsync();
+    Assert.Contains("id", responseString);
+  }
+
+  [Fact]
+  public async Task GetChessGame_Returns_Game_With_Id()
+  {
+    // Arrange: Create new game
+    var createResponse = await _client.PostAsync("/api/chessgames/", null);
+
+    // Assert: Game is created
+    Assert.Equal(HttpStatusCode.Created, createResponse.StatusCode);
+    var createResponseContentString =
+      await createResponse.Content.ReadAsStringAsync();
+    var gameId = ExtractGameId(createResponseContentString);
+
+    // Act: Retrieve game by id
+    var getResponse = await _client.GetAsync($"/api/chessgames/{gameId}");
+
+    // Assert: Created game is retrieved
+    getResponse.EnsureSuccessStatusCode();
+    var getResponseContentString =
+      await getResponse.Content.ReadAsStringAsync();
+    Assert.Contains("id", getResponseContentString);
+    Assert.Contains("turn", getResponseContentString);
+    Assert.Contains("chessMoves", getResponseContentString);
+    var retrievedGameId = ExtractGameId(getResponseContentString);
+    Assert.Equal(retrievedGameId, gameId);
+  }
+
+  [Fact]
+  public async Task GetChessGames_Returns_All_Games()
+  {
+    // Arrange: Create new games
+    var createResponse1 = await _client.PostAsync("/api/chessgames/", null);
+    var createResponse2 = await _client.PostAsync("/api/chessgames/", null);
+
+    // Assert: Games are created
+    Assert.Equal(HttpStatusCode.Created, createResponse1.StatusCode);
+    Assert.Equal(HttpStatusCode.Created, createResponse2.StatusCode);
+    var createResponse1ContentString =
+      await createResponse1.Content.ReadAsStringAsync();
+    var createResponse2ContentString =
+      await createResponse2.Content.ReadAsStringAsync();
+
+    // Act: Retrieve all games (including the created ones above)
+    var getResponse = await _client.GetAsync("/api/chessgames/");
+
+    // Assert: All games are retrieved
+    var getResponseContentString =
+      await getResponse.Content.ReadAsStringAsync();
+    var getResponseJson =
+      JsonSerializer.Deserialize<dynamic>(getResponseContentString);
+    Assert.Equal(JsonValueKind.Array, getResponseJson.ValueKind);
+    Assert.Contains(createResponse1ContentString, getResponseContentString);
+    Assert.Contains(createResponse2ContentString, getResponseContentString);
+  }
+
+  private long ExtractGameId(string responseString)
+  {
+    var responseJson = JsonSerializer.Deserialize<dynamic>(responseString);
+    return responseJson.GetProperty("id").GetInt64();
+  }
+}
