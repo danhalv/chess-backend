53707add5d468532e98576121e31aa0320743f4c
DanielH4 <danne.halvarsson@gmail.com>
2025-02-12 23:17:30 +0100

    refactor Bishop GetMoves function

--- ChessLib/Piece/Bishop.cs
+++ ChessLib/Piece/Bishop.cs
@@ -15,45 +15,57 @@ public class Bishop : IPiece
 
   List<Move> IPiece.GetMoves(Board board, int pieceTilePos)
   {
-    var moves = new List<Move>();
-
-    void addDiagonalTilesUntilOccupied(List<int> diagonalTiles)
+    var diagonals = new List<List<int>>
     {
-      foreach (int tileIndex in diagonalTiles)
+      Tile.DiagonalTiles(Direction.DiagonalRight,
+                         Direction.Forward,
+                         pieceTilePos,
+                         this.Color),
+      Tile.DiagonalTiles(Direction.DiagonalRight,
+                         Direction.Backward,
+                         pieceTilePos,
+                         this.Color),
+      Tile.DiagonalTiles(Direction.DiagonalLeft,
+                         Direction.Forward,
+                         pieceTilePos,
+                         this.Color),
+      Tile.DiagonalTiles(Direction.DiagonalLeft,
+                         Direction.Backward,
+                         pieceTilePos,
+                         this.Color)
+    };
+
+    // add moves in diagonal until a tile is occupied
+    // include occupied tile if it's opponent's piece
+    List<Move> legalMoves(List<int> diagonal)
+    {
+      var moves = new List<Move>();
+
+      foreach (int tileIndex in diagonal)
       {
-        if (!board.IsTileOccupied(tileIndex))
+        IPiece piece = board.GetTile(tileIndex).Piece as IPiece;
+
+        if (piece == null)
         {
           moves.Add(new Move(pieceTilePos, tileIndex));
         }
         else
         {
-          IPiece tilePiece = board.GetTile(tileIndex).Piece!;
-
-          if (tilePiece.Color != this.Color)
+          if (piece.Color != this.Color)
             moves.Add(new Move(pieceTilePos, tileIndex));
 
           break;
         }
       }
+
+      return moves;
     }
 
-    addDiagonalTilesUntilOccupied(Tile.DiagonalTiles(Direction.DiagonalRight,
-                                                     Direction.Forward,
-                                                     pieceTilePos,
-                                                     this.Color));
-    addDiagonalTilesUntilOccupied(Tile.DiagonalTiles(Direction.DiagonalRight,
-                                                     Direction.Backward,
-                                                     pieceTilePos,
-                                                     this.Color));
-    addDiagonalTilesUntilOccupied(Tile.DiagonalTiles(Direction.DiagonalLeft,
-                                                     Direction.Forward,
-                                                     pieceTilePos,
-                                                     this.Color));
-    addDiagonalTilesUntilOccupied(Tile.DiagonalTiles(Direction.DiagonalLeft,
-                                                     Direction.Backward,
-                                                     pieceTilePos,
-                                                     this.Color));
-
-    return moves;
+    return diagonals.Aggregate(new List<Move>(),
+                               (current, next) =>
+    {
+      current.AddRange(legalMoves(next));
+      return current;
+    });
   }
 }
